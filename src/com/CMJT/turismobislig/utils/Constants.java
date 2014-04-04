459a457f13fe1aa8adde449db1df564c54334de1
Angel Abellanosa <auabellanosa@hotmail.com>
2014-04-05 05:45:20 +0800

    a commit to honor new gitignore setup for android generated files

---
+++ src/com/CMJT/turismobislig/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.CMJT.turismobislig.utils;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class Constants {
+	public static int BABANO = 0;
+	
+	public static LatLng[] LATLOCS = {
+			new LatLng(8.18839, 126.35602)
+	};
+	
+	public static String[] TITLES = {
+			"Casa de Babano Restaurant"
+	};
+	
+
+}
